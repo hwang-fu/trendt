ca043063e4115bea83d68a0b5a0ef1214f5999d6
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-28 00:22:46 +0100

    feat: add Torrent struct

---
+++ crates/trendt-torrent/src/torrent.rs
@@ -0,0 +1,23 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Torrent {
+    /// Primary tracker URL
+    pub announce: String,
+
+    /// Optional: backup tracker URLs (list of tiers, each tier is a list of URLs)
+    #[serde(rename = "announce-list")]
+    pub announce_list: Option<Vec<Vec<String>>>,
+
+    /// Optional: Unix timestamp when torrent was created
+    #[serde(rename = "creation date")]
+    pub creation_date: Option<i64>,
+
+    /// Optional: free-form comment
+    pub comment: Option<String>,
+
+    /// Optional: client that created this torrent
+    #[serde(rename = "created by")]
+    pub created_by: Option<String>,
+    // We'll add the `info` field in subphase 3.2
+}
